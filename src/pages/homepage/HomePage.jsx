bd2e7ff07ca2a1f0af65c9fd54f00014e4fb9e52
pitbrest <sasha.petrov@tut.by>
2024-01-09 00:15:10 +0300

    sc

--- src/pages/homepage/HomePage.jsx
+++ src/pages/homepage/HomePage.jsx
@@ -1,35 +1,48 @@
-import { useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { Container } from "../../components/Container";
 import "./HomePage.css";
 import MontagPS from "../../assets/images/slider/montag.jpeg";
 import PlanEvac from "../../assets/images/slider/plan.png";
 
+const images = [MontagPS, PlanEvac];
+
 function HomePage() {
-  const [activeImg, setActiveImg] = useState(1);
-  const activityToggler = (id) => setActiveImg(id);
+  const [idx, setIdx] = useState(0);
+  const imgToggler = () => (idx !== images.length - 1 ? setIdx(idx + 1) : setIdx(0));
+  const currentImg = images[idx];
+  const imgRef = useRef(null);
+
+  useEffect(() => {
+    const effectOneTimeout = setTimeout(() => imgRef.current.classList.add("fadeIn"), 0);
+    const effectTwoTimeout = setTimeout(() => imgRef.current.classList.add("fadeOut"), 4300);
+    const imgInterval = setInterval(() => {
+      imgRef.current.classList.remove("fadeIn");
+      imgRef.current.classList.remove("fadeOut");
+      imgToggler();
+    }, 5000);
+    return () => {
+      clearTimeout(effectOneTimeout);
+      clearTimeout(effectTwoTimeout);
+      clearInterval(imgInterval);
+    };
+  });
 
   return (
     <>
       <Container>
-        HomePage
         <div className="slider-container">
           <div className="slider-wrapper flex flex-col justify-center items-center gap-8">
             <div className="slides relative top-0 left-0 w-5/6 overflow-hidde">
-              <div className="inline-flex gap-20">
+              <div className="my-0 mx-auto max-w-lg overflow-hidden">
                 <img
-                  className=""
-                  src={MontagPS}
-                  alt="presentation"
-                />
-
-                <img
-                  className=""
-                  src={PlanEvac}
+                  ref={imgRef}
+                  className="slider-img"
+                  src={currentImg}
                   alt="presentation"
                 />
               </div>
             </div>
-            <div className="navigation flex justify-center items-center gap-5">
+            {/* <div className="navigation flex justify-center items-center gap-5">
               <div
                 className="w-3 h-3 rounded-xl border-[3px] border-transparent outline outline-[--bgc-dark] outline-[3px] outline-offset-[3px] active"
                 id="1"
@@ -50,7 +63,7 @@ function HomePage() {
                 className="w-3 h-3 rounded-xl border-[3px] border-transparent outline outline-[--bgc-dark] outline-[3px] outline-offset-[3px]"
                 id="5"
               ></div>
-            </div>
+            </div> */}
           </div>
         </div>
       </Container>
