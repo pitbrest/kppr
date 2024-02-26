33f81681a63d061787ced1762a59dc0082dc7690
pitbrest <sasha.petrov@tut.by>
2024-02-26 07:22:57 +0300

    sc

--- src/pages/homepage/HomePage.jsx
+++ src/pages/homepage/HomePage.jsx
@@ -6,7 +6,7 @@ import { motion } from "framer-motion";
 
 import { Slider } from "../../components/Slider";
 import { sliderData, createSlide } from "../../appData/sliderData";
-import { TbArrowBigDownLinesFilled } from "react-icons/tb";
+import { FaArrowDownLong } from "react-icons/fa6";
 import {
   advantagesData,
   createAdvantages,
@@ -18,7 +18,7 @@ function HomePage() {
   return (
     <>
       <Container>
-        <section className="flex flex-col justify-start items-center">
+        <section className="flex flex-col justify-start items-center mb-60">
           <img
             className="w-auto max-h-[60%]"
             src={MainImg}
@@ -28,31 +28,35 @@ function HomePage() {
             <p className="text-center">*** комплексные решения в сфере пожарной безопасности</p>
           </div>
         </section>
-        <section className="my-24">
+
+        {/* <section className="my-24 flex items-start justify-center gap-4">
           <motion.div
-            className="flex items-center justify-center"
             initial={{ opacity: 0, y: 100 }}
             animate={{ opacity: 1, y: 0 }}
-            transition={{ delay: 1 }}
+            transition={{ delay: 2 }}
           >
-            <TbArrowBigDownLinesFilled
-              size={70}
-              color="#dd3b39"
-            />
-            <TbArrowBigDownLinesFilled
-              size={70}
-              color="#dd3b39"
-            />
-            <TbArrowBigDownLinesFilled
-              size={70}
-              color="#dd3b39"
-            />
+            <FaArrowDownLong size={30} />
           </motion.div>
-        </section>
+          <motion.div
+            initial={{ opacity: 0, y: 100 }}
+            animate={{ opacity: 1, y: 0 }}
+            transition={{ delay: 2.2 }}
+          >
+            <FaArrowDownLong size={40} />
+          </motion.div>
+          <motion.div
+            initial={{ opacity: 0, y: 100 }}
+            animate={{ opacity: 1, y: 0 }}
+            transition={{ delay: 2.3 }}
+          >
+            <FaArrowDownLong size={30} />
+          </motion.div>
+        </section> */}
+
         <section className="slider min-[900px]:h-[60vh] h-auto">
           <Slider>{createSlide(sliderData)}</Slider>
         </section>
-        <section className="mb-14 mt-10">
+        {/* <section className="mb-14 mt-10">
           <motion.div
             className="flex items-center justify-center"
             initial={{ opacity: 0, y: 100 }}
@@ -73,14 +77,23 @@ function HomePage() {
               color="#dd3b39"
             />
           </motion.div>
-        </section>
+        </section> */}
         <section className="mb-10">
-          <h3 className="text-center min-[594px]:text-6xl text-4xl font-semibold my-20 pt-3 pb-5 px-2 bg-[--bgc-gold] rounded-md">
-            Наши преимущества
-          </h3>
+          <motion.div
+            className="flex items-center justify-center"
+            initial={{ opacity: 0, y: 100 }}
+            whileInView={{ opacity: 1, y: 0 }}
+            viewport={{ once: true, amount: 0.8 }}
+            transition={{ duration: 0.5, delay: 0 }}
+          >
+            <h3 className="text-center min-[594px]:text-6xl text-4xl font-semibold my-20 pt-3 pb-5 px-2 bg-[--bgc-gold] rounded-md">
+              Наши преимущества
+            </h3>
+          </motion.div>
+
           {createAdvantages(advantagesData)}
         </section>
-        <section className="my-24">
+        {/* <section className="my-24">
           <motion.div
             className="flex items-center justify-center"
             initial={{ opacity: 0, y: 100 }}
@@ -101,7 +114,7 @@ function HomePage() {
               color="#dd3b39"
             />
           </motion.div>
-        </section>
+        </section> */}
         <section>
           <h3 className="text-center min-[594px]:text-6xl text-4xl font-semibold my-20 pt-3 pb-5 px-2 bg-[--bgc-gold] rounded-md">
             Наши результаты
