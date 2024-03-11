8942431ccd9602f758115e58abbbcf8f9014407c
pitbrest <sasha.petrov@tut.by>
2024-03-11 08:45:48 +0300

    sc

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ import App from "./App";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <BrowserRouter basename="https://pitbrest.github.io/kppr/">
+    <BrowserRouter>
       <Provider store={store}>
         <App />
       </Provider>
