731579be5aab2508f62b5dc71bf071f3c209fdbe
pitbrest <sasha.petrov@tut.by>
2024-05-30 10:14:40 +0300

    sc

--- src/appData/homePageData.jsx
+++ src/appData/homePageData.jsx
@@ -6,6 +6,8 @@ import { PageNavigation } from "../components/page-navigation/PageNavigation";
 
 import CountUpComponent from "../components/countUp/CountUpComponent";
 import { motion } from "framer-motion";
+import { servicesData } from "./servicesData";
+import { FaStarOfLife } from "react-icons/fa";
 
 const advantagesData = [
   { advantage: "более 20 лет на рынке строительных работ и услуг", img: IoBarChart },
@@ -39,6 +41,43 @@ const resultsData = [
   },
 ];
 // const delay = [0, 0.3, 0.5, 0.7, 0.9, 1.1];
+
+const createWorks = (data) => {
+  return data.production.map((item) => (
+    <li
+      className="flex items-center justify-between mb-3"
+      key={item.split(",")[0] || item}
+    >
+      <div className="flex items-center justify-start min-[576px]:gap-6 gap-3 w-full">
+        <FaStarOfLife
+          size={15}
+          className="mt-1"
+        />
+        <h2 className="min-[576px]:text-lg text-base block min-[576px]:max-w-[95%] max-w-[90%]">
+          {item};
+        </h2>
+      </div>
+    </li>
+  ));
+};
+const createServices = (data) => {
+  return data.services.map((item) => (
+    <li
+      className="flex items-center justify-between mb-3"
+      key={item.split(",")[0] || item}
+    >
+      <div className="flex items-center justify-start min-[576px]:gap-6 gap-3 w-full">
+        <FaStarOfLife
+          size={15}
+          className="mt-1"
+        />
+        <h2 className="min-[576px]:text-base text-sm block min-[576px]:max-w-[95%] max-w-[90%]">
+          {item};
+        </h2>
+      </div>
+    </li>
+  ));
+};
 
 const createAdvantages = (data) => {
   return (
@@ -92,4 +131,12 @@ const createResults = (data) => {
   );
 };
 
-export { advantagesData, createAdvantages, resultsData, createResults };
+export {
+  advantagesData,
+  createAdvantages,
+  resultsData,
+  createResults,
+  createWorks,
+  createServices,
+  servicesData,
+};
