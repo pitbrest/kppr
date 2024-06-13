e12c4ae4dabe490950ec1259d10b83ba59f94be1
pitbrest <sasha.petrov@tut.by>
2024-06-13 14:43:10 +0300

    sc

--- src/appData/bobsData.jsx
+++ src/appData/bobsData.jsx
@@ -1,6 +1,12 @@
+import sertBobs from "../assets/documents/sert_bobs.pdf";
+import reglamentBobs from "../assets/documents/reglament_bobs.pdf";
+
 const bobsData = [
-  { img: "", title: "Сертификат соответствия" },
-  { img: "", title: "Регламент применения" },
+  {
+    img: sertBobs,
+    title: "Сертификат соответствия ЕАЭС",
+  },
+  { img: reglamentBobs, title: "Регламент применения" },
 ];
 
 export default bobsData;
